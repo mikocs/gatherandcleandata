501ae831c823ee56bf1ee20788faa4e362729846
Csaba Miko <mikocs79@gmail.com>
2016-07-09 23:00:20 +0200

    This is the initial commit to the Gather and Clean Data repository

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gatherandcleandata
+# Gather and Clean Data
+
+This is the Coursera Gather and Clean data course assignment script.
---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+## Coursera Getting and Cleaning Data course assignment
+
+## the following script downloads a zipped dataset on 
+
+run_analysis <- function() {
+        a <<- ":"
+}
